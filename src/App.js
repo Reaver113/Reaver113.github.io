d452639cf5c99b7e22468f3f00a67e5c1be5a18f
Reaver113 <jasonalcock113@gmail.com>
2023-01-16 00:43:22 +1000

    hash router added, links changed to fit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import {Navigate} from "react-router-dom"
 import Home from "./components/Home"
 import Contact from "./components/Contact"
