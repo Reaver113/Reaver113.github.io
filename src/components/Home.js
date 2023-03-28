164aae5f97399e8b6e527658d374394f086e1b12
Reaver113 <jasonalcock113@gmail.com>
2023-03-28 15:43:06 +1000

    fixed for deployment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,11 @@
 import React from "react"
 import Pfp from "../img/Pfp.jpg"
 import './Home.css'
-import Background from "./Background"
 
 const Home = () => {
     return(
         <main>
-        <img src={Pfp} class="pfp"></img>
+        <img src={Pfp} alt="pfp" class="pfp"></img>
         <h1>Jason Alcock</h1>
         <p class="about">
         I'm a Gold Coast / Brisbane based Junior Developer with a passion for design and development, currently looking for work experience to help broaden my skills and advance my knowledge within the industry!<br /><br />
